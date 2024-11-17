7db346e6160e26ec55d3b9ebc2faf87347a14fd5
1yohanyo1 <yohangim119@gmail.com>
2024-11-17 17:30:48 +0900

    oop study

---
+++ src/class/example_class.cpp
@@ -0,0 +1,20 @@
+// class gs_engine : public ic_engine{
+// public:
+//     gs_engine(); // 생성자
+//     ~gs_engine(); // 소멸자
+
+//     int get_current_fuel() { return current_fuel;};
+
+// private:
+//     void acceleration_output() override;
+//     void increasing_fuel() { increasing_pistion_speed(); };
+//     void increasing_piston_speed() {};
+
+//     int current_fuel;
+//     int piston_speed;
+// };
+
+// void gs_engine::acceleration_output(){
+//     increasing_fuel();
+//     current_fuel++;
+// }
